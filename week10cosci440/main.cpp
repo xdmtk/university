21f14e688531f19cb50b7e0bfd2ac573b78efec3
Nick Martinez <nick@xdmtk.org>
2018-04-17 19:35:25 -0700

    Classes in lab

---
+++ week10cosci440/main.cpp
@@ -0,0 +1,106 @@
+/*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+!									     #
+#									     !
+!	Programmer: 	Nick Martinez					     #
+#	Class:		COSCI 440 - TUE 5:45 - 10:00PM			     !
+!	Instructor:	Mari Rettke					     #
+#	Date:		4/13/2018					     !
+!									     #
+#	                Lab Assignment: Namespaces                           !	
+!									     #
+#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+
+
+		  _             _           _            _              _     
+		/\ \           /\ \        / /\        /\ \            /\ \
+	       /  \ \         /  \ \      / /  \      /  \ \           \ \ \
+	      / /\ \ \       / /\ \ \    / / /\ \__  / /\ \ \          /\ \_\
+	     / / /\ \ \     / / /\ \ \  / / /\ \___\/ / /\ \ \        / /\/_/ 
+	    / / /  \ \_\   / / /  \ \_\ \ \ \ \/___/ / /  \ \_\      / / /    
+	   / / /    \/_/  / / /   / / /  \ \ \    / / /    \/_/     / / /     
+	  / / /          / / /   / / /    \ \ \  / / /             / / /      
+	 / / /________  / / /___/ / /_/\__/ / / / / /________  ___/ / /__     
+	/ / /_________\/ / /____\/ /\ \/___/ / / / /_________\/\__\/_/___\
+	\/____________/\/_________/  \_____\/  \/____________/\/_________/    
+
+
+
+			       _            _            _          
+			   _  /\ \      _  /\ \        / /\
+			  /\_\\ \ \    /\_\\ \ \      / /  \
+			 / / / \ \ \  / / / \ \ \    / / /\ \
+			/ / /   \ \ \/ / /   \ \ \  / / /\ \ \
+			\ \ \____\ \ \ \ \____\ \ \/_/ /  \ \ \
+			 \ \________\ \ \________\ \ \ \   \ \ \
+			  \/________/\ \/________/\ \ \ \   \ \ \
+				    \ \ \        \ \ \ \ \___\ \ \
+				     \ \_\        \ \_\ \/____\ \ \
+				      \/_/         \/_/\_________\/
+                
+
+
+                                      THIS IS THE SOURCE FILE
+                                      DEFINING ALL FUNCTIONS
+
+
+
+
+#! In this exercise you will declare a class with a method and instatiate
+    an object of the class.
+
+    There will be two files
+    One file will be a public class called GradeBook. This class will have one method
+    called displayMessage() that displays a message to the GradeBook user
+
+    The messege this class displays will be "Welcome to the Grade Book"
+
+    The second file will be the file that contains the main()
+
+    This will create a new GradeBook object called myGradeBook and use it to 
+    call the displayMessage() method of the GradeBook class
+
+
+
+        
+
+
+
+
+
+
+(Section is commented out via the extended line comment syntax rather than //)
+/////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+*///////////////////////////////////////////////////////////////////////////
+
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cfloat>
+
+#include "gradebook.h"
+
+
+
+int main()
+{
+    GradeBook gbobj;
+    std::string courseName;
+
+
+    std::cout << "Enter a coursename: ";
+    std::cin >> courseName;
+
+    
+    gbobj.displayMessage(courseName);
+
+
+    return 0;
+
+
+}
