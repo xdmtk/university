274da39f2a0c45bf051b113b24a55d1c87d5cfb8
Nick Martinez <nick@xdmtk.org>
2018-05-08 21:08:17 -0700

    week 14 in class

---
+++ week14cosci440/main.cpp
@@ -0,0 +1,110 @@
+/*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+!									     #
+#									     !
+!	Programmer: 	Nick Martinez					     #
+#	Class:		COSCI 440 - TUE 5:45 - 10:00PM			     !
+!	Instructor:	Mari Rettke					     #
+#	Date:		5/8/2018					     !
+!									     #
+#	       Abstract Array Class Main File                                ! 
+!                               	   				     #
+#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+
+
+
+		  _             _           _            _              _     
+		/\ \           /\ \        / /\        /\ \            /\ \
+	       /  \ \         /  \ \      / /  \      /  \ \           \ \ \
+	      / /\ \ \       / /\ \ \    / / /\ \__  / /\ \ \          /\ \_\
+	     / / /\ \ \     / / /\ \ \  / / /\ \___\/ / /\ \ \        / /\/_/ 
+	    / / /  \ \_\   / / /  \ \_\ \ \ \ \/___/ / /  \ \_\      / / /    
+	   / / /    \/_/  / / /   / / /  \ \ \    / / /    \/_/     / / /     
+	  / / /          / / /   / / /    \ \ \  / / /             / / /      
+	 / / /________  / / /___/ / /_/\__/ / / / / /________  ___/ / /__     
+	/ / /_________\/ / /____\/ /\ \/___/ / / / /_________\/\__\/_/___\
+	\/____________/\/_________/  \_____\/  \/____________/\/_________/    
+
+
+
+			       _            _            _          
+			   _  /\ \      _  /\ \        / /\
+			  /\_\\ \ \    /\_\\ \ \      / /  \
+			 / / / \ \ \  / / / \ \ \    / / /\ \
+			/ / /   \ \ \/ / /   \ \ \  / / /\ \ \
+			\ \ \____\ \ \ \ \____\ \ \/_/ /  \ \ \
+			 \ \________\ \ \________\ \ \ \   \ \ \
+			  \/________/\ \/________/\ \ \ \   \ \ \
+				    \ \ \        \ \ \ \ \___\ \ \
+				     \ \_\        \ \_\ \/____\ \ \
+				      \/_/         \/_/\_________\/
+
+
+
+#! Write a program that will allow the user to type in a line of text. The line
+    the user types in should be printed centered in the page
+
+                    MAIN FILE
+
+
+
+(Section is commented out via the extended line comment syntax rather than //)
+/////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+*///////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <iomanip>
+#include "abstarr.h"
+
+
+
+#define SIZE 20
+
+
+int main()
+{
+
+
+    // Create integer list object
+    IntegerList test(SIZE);
+
+    // Create temporary variable to hold return value
+    // of getElement
+    int x;
+
+
+    for (int i = 0; i < 20; ++i)
+    {
+        // If the element is sucessfully able to be set
+        // then print out the asterisj 
+        if (test.setElement(i,9))
+        {
+            std::cout << "*" << std::endl;
+        }
+        else
+        {
+
+    }
+
+
+
+
+
+    for (int i = 0; i < 20; ++i)
+    {
+        x  = test.getElement(i);
+        std::cout << x << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
