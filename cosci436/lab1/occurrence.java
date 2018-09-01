f1e1ad56cb19b842b8ae62c8eabd98b3ca98c043
Nick <nick@xdmtk.org>
2018-09-01 08:34:57 -0700

    Add output to src file

--- cosci436/lab1/occurrence.java
+++ cosci436/lab1/occurrence.java
@@ -194,3 +194,120 @@ public class occurrence {
 
 }
 
+
+
+
+// Output 
+/*
+* * * * * * * * * * * * * * * * * *
+*  Number: 01  -  Occurence: 11   *
+*  Number: 02  -  Occurence: 13   *
+*  Number: 03  -  Occurence: 10   *
+*  Number: 04  -  Occurence: 08   *
+*  Number: 05  -  Occurence: 13   *
+*  Number: 06  -  Occurence: 07   *
+*  Number: 07  -  Occurence: 09   *
+*  Number: 08  -  Occurence: 10   *
+*  Number: 09  -  Occurence: 09   *
+*  Number: 10  -  Occurence: 15   *
+*  Number: 11  -  Occurence: 09   *
+*  Number: 12  -  Occurence: 06   *
+*  Number: 13  -  Occurence: 07   *
+*  Number: 14  -  Occurence: 10   *
+*  Number: 15  -  Occurence: 06   *
+*  Number: 16  -  Occurence: 11   *
+*  Number: 17  -  Occurence: 12   *
+*  Number: 18  -  Occurence: 12   *
+*  Number: 19  -  Occurence: 06   *
+*  Number: 20  -  Occurence: 11   *
+*  Number: 21  -  Occurence: 09   *
+*  Number: 22  -  Occurence: 13   *
+*  Number: 23  -  Occurence: 04   *
+*  Number: 24  -  Occurence: 08   *
+*  Number: 25  -  Occurence: 03   *
+*  Number: 26  -  Occurence: 09   *
+*  Number: 27  -  Occurence: 12   *
+*  Number: 28  -  Occurence: 10   *
+*  Number: 29  -  Occurence: 08   *
+*  Number: 30  -  Occurence: 08   *
+*  Number: 31  -  Occurence: 09   *
+*  Number: 32  -  Occurence: 17   *
+*  Number: 33  -  Occurence: 08   *
+*  Number: 34  -  Occurence: 17   *
+*  Number: 35  -  Occurence: 15   *
+*  Number: 36  -  Occurence: 10   *
+*  Number: 37  -  Occurence: 06   *
+*  Number: 38  -  Occurence: 13   *
+*  Number: 39  -  Occurence: 12   *
+*  Number: 40  -  Occurence: 09   *
+*  Number: 41  -  Occurence: 08   *
+*  Number: 42  -  Occurence: 11   *
+*  Number: 43  -  Occurence: 09   *
+*  Number: 44  -  Occurence: 10   *
+*  Number: 45  -  Occurence: 13   *
+*  Number: 46  -  Occurence: 10   *
+*  Number: 47  -  Occurence: 09   *
+*  Number: 48  -  Occurence: 06   *
+*  Number: 49  -  Occurence: 09   *
+*  Number: 50  -  Occurence: 11   *
+*  Number: 51  -  Occurence: 04   *
+*  Number: 52  -  Occurence: 10   *
+*  Number: 53  -  Occurence: 13   *
+*  Number: 54  -  Occurence: 07   *
+*  Number: 55  -  Occurence: 05   *
+*  Number: 56  -  Occurence: 21   *
+*  Number: 57  -  Occurence: 09   *
+*  Number: 58  -  Occurence: 08   *
+*  Number: 59  -  Occurence: 08   *
+*  Number: 60  -  Occurence: 10   *
+*  Number: 61  -  Occurence: 11   *
+*  Number: 62  -  Occurence: 09   *
+*  Number: 63  -  Occurence: 09   *
+*  Number: 64  -  Occurence: 10   *
+*  Number: 65  -  Occurence: 11   *
+*  Number: 66  -  Occurence: 14   *
+*  Number: 67  -  Occurence: 07   *
+*  Number: 68  -  Occurence: 09   *
+*  Number: 69  -  Occurence: 08   *
+*  Number: 70  -  Occurence: 10   *
+*  Number: 71  -  Occurence: 11   *
+*  Number: 72  -  Occurence: 12   *
+*  Number: 73  -  Occurence: 17   *
+*  Number: 74  -  Occurence: 07   *
+*  Number: 75  -  Occurence: 10   *
+*  Number: 76  -  Occurence: 15   *
+*  Number: 77  -  Occurence: 16   *
+*  Number: 78  -  Occurence: 06   *
+*  Number: 79  -  Occurence: 11   *
+*  Number: 80  -  Occurence: 09   *
+*  Number: 81  -  Occurence: 07   *
+*  Number: 82  -  Occurence: 05   *
+*  Number: 83  -  Occurence: 15   *
+*  Number: 84  -  Occurence: 07   *
+*  Number: 85  -  Occurence: 09   *
+*  Number: 86  -  Occurence: 09   *
+*  Number: 87  -  Occurence: 15   *
+*  Number: 88  -  Occurence: 15   *
+*  Number: 89  -  Occurence: 08   *
+*  Number: 90  -  Occurence: 08   *
+*  Number: 91  -  Occurence: 12   *
+*  Number: 92  -  Occurence: 14   *
+*  Number: 93  -  Occurence: 11   *
+*  Number: 94  -  Occurence: 09   *
+*  Number: 95  -  Occurence: 11   *
+*  Number: 96  -  Occurence: 10   *
+*  Number: 97  -  Occurence: 16   *
+*  Number: 98  -  Occurence: 09   *
+*  Number: 99  -  Occurence: 12   *
+* * * * * * * * * * * * * * * * * *
+*/
+
+
+
+
+
+
+
+
+
+
