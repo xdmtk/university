c8aa515ef33846ec88bbf433d9837a737761ea20
Nick <nick@xdmtk.org>
2018-09-05 10:10:29 -0700

    Notes 9/5/18

---
+++ cosci436/lab2/person.java
@@ -0,0 +1,149 @@
+
+    /*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+    !                                                                            #
+    #                                                                            !
+    !   Programmer:     Nick Martinez                                            #
+    #   Class:          COSCI 436 - SAT 8:15 - 10:30AM                           !
+    !   Instructor:     Milan Samplewala                                         #
+    #   Date:           8/28/2018                                                !
+    !                                                                            #
+    #          LAB 2 -  Classes/Inheritence                                      !
+    !                                                                            #
+    #!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+
+
+
+________/\\\\\\\\\_______/\\\\\__________/\\\\\\\\\\\__________/\\\\\\\\\__/\\\\\\\\\\\_        
+_____/\\\////////______/\\\///\\\______/\\\/////////\\\_____/\\\////////__\/////\\\///__       
+___/\\\/_____________/\\\/__\///\\\___\//\\\______\///____/\\\/_______________\/\\\_____      
+__/\\\______________/\\\______\//\\\___\////\\\__________/\\\_________________\/\\\_____     
+_\/\\\_____________\/\\\_______\/\\\______\////\\\______\/\\\_________________\/\\\_____    
+_\//\\\____________\//\\\______/\\\__________\////\\\___\//\\\________________\/\\\_____   
+__\///\\\___________\///\\\__/\\\_____/\\\______\//\\\___\///\\\______________\/\\\_____  
+____\////\\\\\\\\\____\///\\\\\/_____\///\\\\\\\\\\\/______\////\\\\\\\\\__/\\\\\\\\\\\_ 
+_______\/////////_______\/////_________\///////////___________\/////////__\///////////__
+
+
+ _______________________/\\\________/\\\\\\\\\\_____________/\\\\\____________________
+  ____________________/\\\\\______/\\\///////\\\________/\\\\////____________________
+ ___________________/\\\/\\\_____\///______/\\\______/\\\///___________________________
+  ________________/\\\/\/\\\____________/\\\//_____/\\\\\\\\\\\_______________________
+ _______________/\\\/__\/\\\___________\////\\\___/\\\\///////\\\______________________
+  ____________/\\\\\\\\\\\\\\\\___________\//\\\_\/\\\______\//\\\____________________
+ ____________\///////////\\\//___/\\\______/\\\__\//\\\______/\\\___________________
+   ____________________\/\\\____\///\\\\\\\\\/____\///\\\\\\\\\/______________________
+  _____________________\///_______\/////////________\/////////______________________
+
+
+        Count occurence of numbers: 
+
+           Create sub class: Student and Staff - of Person
+
+           Create sub class: Administration and Faculty - of Staff
+
+                enum Status: Registered, Enrolled, InActive and Archived
+                
+                Student extends Person
+                    Status status
+                    -- other relevant methods. 
+                    -- use super
+                
+                Staff extends Person
+                double salary
+                    Date hiredate
+                    -- Constructor(s) - using super
+                    -- setters and getters
+                    -- toString()
+                
+                Administrator extends Staff
+                    String position
+                    String dept
+                    -- Constructor(s) - using super
+                    -- setters and getters
+                    -- toString()
+                
+                Faculty extends Staff
+                    ArrayList courses
+                    -- Constructor(s) - using super
+                    - setters and getters
+                    -- toString()
+
+
+
+*/
+
+
+import java.lang.Enum.*;
+
+
+
+public class Person {
+
+    public enum  {
+        Registered, Enrolled, InActive, Archived;
+    }
+
+
+
+}
+
+
+
+public class Student extends Person {
+    
+    Status status;
+    
+    // Other relevant methods...
+    // User super in some methods..
+
+}
+
+
+
+public class Staff extends Person {
+
+    private  Date hiredate;
+    private double salary;
+    
+    public Staff() {
+
+        // Constructor for Staff class
+
+
+
+    }
+
+
+
+    private void setDate(Date hd) {
+
+        this.hiredate = hd;
+
+
+    }
+
+
+    private Date getDate() {
+
+        return this.hiredate;
+    }
+
+
+    private void setSalary(double s) {
+        
+        this.salary = s;
+
+    }
+
+    private double getSalary() {
+
+        return this.salary;
+    }
+
+
+
+    private void toString() {
+
+
+
+    }
