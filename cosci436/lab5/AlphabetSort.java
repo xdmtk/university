8f0748ec19ed2806ee68f1016251e89cc493db98
Nick <nick@xdmtk.org>
2018-10-13 09:42:13 -0700

    Add docs

--- cosci436/lab5/AlphabetSort.java
+++ cosci436/lab5/AlphabetSort.java
@@ -59,32 +59,41 @@ public class AlphabetSort {
 
     
     public static void main(String[] args) {
-        
+       
+
+        // Error check for number of command line args
         if (args.length != 1) {
             System.out.println("AlphabetSort requires one argument - Arguments given: " + args.length);
             return;
         }
-
+        
+        // If input file supplied, continue with object creation
         AlphabetSort wl = new AlphabetSort();
+
+        // Use the path supplied by argument to set objects path
         if (!wl.setFilePath(args[0])) {
+
+            // Error check for invalid paths
             System.out.println("File path given does not exist: " + args[0]);
             return;
         }
-        
+       
+        // Parse file word by word
         if (!wl.parseFile()) {
+
+            // Error checking on parse file
             System.out.println("Failed to parse file");
             return;
         }
+        
 
+        // Parsed list to string before sort
         System.out.println("Before sort:\n" + wl.toString());
         wl.sortList();
 
+
+        // Parsed list to string after sort
         System.out.println("\n\nAfter sort:\n" + wl.toString());
-
-
-
-
-
 
         return;
     }
@@ -94,11 +103,13 @@ public class AlphabetSort {
 
     public boolean setFilePath(String fp) {
         
+        // Check for file existence
         File f = new File(fp);
         if (!f.exists()) {
             return false;
         }
         
+        // Set file path property if file exists
         this.filePath = fp;
         this.fpSet = true;
         return true;
@@ -107,19 +118,30 @@ public class AlphabetSort {
 
     public boolean parseFile() {
         
+        // Make sure file path is set before attempting read
         if (!this.fpSet) {
             System.out.println("File path must be set before parsing");
             return false;
         }
-       
+        
+
+        // Try catch because compiler does not know whether file exists
+        // yet or not, throws FileFoundFound IOException without try/catch block
         try {
+            
+            // Create file object to supply to scanner object as source
             File p = new File(this.filePath);
+
+            // Use file object for source in Scanner Constructor
             Scanner r = new Scanner(p);
+
+            // Check first hasNext() method to determine if file is empty
             if (!r.hasNext()) {
                 System.out.println("File is empty");
                 return false;
             }
-            
+           
+            // Continue to grab tokens (words) until file is empty
             while (r.hasNext()) {
                 String t = r.next();
                 this.wordList.add(t);
@@ -135,14 +157,20 @@ public class AlphabetSort {
 
     public boolean sortList() {
 
+
+        // Use static Collection method sort() to sort list
+        // according to natural order of the elements
         if (!this.wordList.isEmpty()) {
             java.util.Collections.sort(this.wordList); 
             return true;
         }
+
         return false;
     }
      
-    
+   
+
+    // Override on toString to print object contents in custom format
     @Override
     public String toString() {
 
