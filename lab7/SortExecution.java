4e37be97f57310229ba0e461ce0f8ae6296b5009
Nick <nick@xdmtk.org>
2018-11-03 15:05:44 -0700

    ddkdkd

--- lab7/SortExecution.java
+++ lab7/SortExecution.java
@@ -125,6 +125,7 @@ public class SortExecution {
             return execTime;
         }
 
+
     public void myMerge(int[] list1, int[] list2, int[] temp) {
         int current1,current2,current3;
         current1 = current2 = current3 = 0;
@@ -150,9 +151,57 @@ public class SortExecution {
     }
 
 
+    public long myQuickSort(int[] list, int first, int last) {
+            long startTime, endTime, execTime;
+            startTime = System.nanoTime();
+                
+            if (last > first) {
+                int pivotIndex = partition(list, first, last);
+                quickSort(list, first, pivotIndex - 1);
+                quickSort(list, pivotIndex + 1, last);
+            } 
+            
+            endTime = System.nanoTime();
+            execTime = endTime - startTime;
+            
+            return execTime;
+        }
 
 
 
+        /** Partition the array list[first..last] */
+        public int partition(int[] list, int first, int last) {
+            int pivot = list[first]; // Choose the first element as the pivot
+            int low = first + 1; // Index for forward search
+            int high = last; // Index for backward search
+            while (high > low) {
+                // Search forward from left
+                while (low <= high && list[low] <= pivot)
+                    low++;
+                    // Search backward from right
+                    while (low <= high && list[high] > pivot)
+                        high--;
+                        // Swap two elements in the list
+                        if (high > low) {
+                            int temp = list[high];
+                            list[high] = list[low];
+                            list[low] = temp;
+                        }
+            }
+            while (high > first && list[high] >= pivot)
+                high--;
+                // Swap pivot with list[high]
+                if (pivot > list[high]) {
+                    list[first] = list[high];
+                    list[high] = pivot;
+                    return high;
+                }
+                else {
+                    return first;
+                }
+        }
+    
+}
 
 
 
