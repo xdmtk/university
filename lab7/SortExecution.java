a0fc37bfdddc2d867dfbf042bc5131dc05fd6b60
Nick <nick@xdmtk.org>
2018-11-11 14:31:50 -0800

    add other sort methods

--- lab7/SortExecution.java
+++ lab7/SortExecution.java
@@ -390,6 +390,122 @@ public class SortExecution {
         }
     }
 
+
+
+    public void insertionSort(int[] list) {
+
+        long startTime, endTime, execTime;
+        startTime = System.nanoTime();
+        
+        
+        for (int i = 1; i < list.length; i++) {
+            /** Insert list[i] into a sorted sublist list[0..i-1] so that
+            list[0..i] is sorted. */
+            int currentElement = list[i];
+            int k;
+            for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
+                list[k + 1] = list[k];
+            }
+            // Insert the current element into list[k + 1]
+            list[k + 1] = currentElement;
+        }
+
+
+        // Calculate total exec time
+        endTime = System.nanoTime();
+        execTime = endTime - startTime;
+
+        return execTime;
+
+
+
+    }
+
+    public void bubbleSort(int[] list) {
+        long startTime, endTime, execTime;
+        startTime = System.nanoTime();
+        
+        
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            // Array may be sorted and next pass not needed
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    // Swap list[i] with list[i + 1]
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true; // Next pass still needed
+                }
+            }
+        }
+        endTime = System.nanoTime();
+        execTime = endTime - startTime;
+
+        return execTime;
+    }
+
+    /** Merge two sorted lists */
+    public static void merge(int[]
+    int current1 = 0; // Current
+    int current2 = 0; // Current
+    int current3 = 0; // Current
+    list1, int[] list2, int[] temp) {
+        index in list1
+        index in list2
+        index in temp
+        while (current1 < list1.length && current2 < list2.length) {
+            if (list1[current1] < list2[current2])
+                temp[current3++] = list1[current1++];
+                else
+                    temp[current3++] = list2[current2++];
+        }
+        while (current1 < list1.length)
+            temp[current3++] = list1[current1++];
+            while (current2 < list2.length)
+                temp[current3++] = list2[current2++];
+    }
+
+
+    public void mergeSort(int[] list) {
+       if (list.length > 1) {
+           // Merge sort the first half
+           int[] firstHalf = new int[list.length / 2];
+           System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+           mergeSort(firstHalf);
+           // Merge sort the second half
+           int secondHalfLength = list.length - list.length / 2;
+           int[] secondHalf = new int[secondHalfLength];
+           System.arraycopy(list, list.length / 2,
+           secondHalf, 0, secondHalfLength);
+           mergeSort(secondHalf);
+           // Merge firstHalf with secondHalf into list
+           merge(firstHalf, secondHalf, list);
+       }
+    }
+
+
+
+
+    public static <E extends Comparable<E>> void heapSort(E[] list) {
+        // Create a Heap of integers
+        Heap<E> heap = new Heap<>();
+        // Add elements to the heap
+        for (int i = 0; i < list.length; i++)
+            heap.add(list[i]);
+            // Remove elements from the heap
+        for (int i = list.length - 1; i >= 0; i--)
+            list[i] = heap.remove();
+    }
+
+
+
+
+
+
+
+
 }
 
 
