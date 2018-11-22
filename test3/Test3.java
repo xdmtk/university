6517f915e9af5fe253753a00b826aff5fcd595c7
Nick <nick@xdmtk.org>
2018-11-21 17:46:38 -0800

    done

--- test3/Test3.java
+++ test3/Test3.java
@@ -59,11 +59,43 @@ _______\/////////_______\/////_________\///////////___________\/////////__\/////
 
         Hint: Counting the number of occurrences of those numbers may lead to a sorting algorithm. 
 
-        (a) Device an algorithm to sort this list of 100 values.
+CHECK        (a) Device an algorithm to sort this list of 100 values.
 
-        (b) Using Big O notation, what is the the efficiency of your algorithm.
+        (b) Using Big O notation, what is the the efficiency of your algorithm. 
 
-        (c) Write the java code to implement the above algorithm.
+                BIG 0 = n*log(n)
+
+CHECK        (c) Write the java code to implement the above algorithm.
+
+
+
+
+        OUTPUT 
+
+            xdmtk@DMT~/prog/fall2018/test3 (master)
+            └─ $ ▶ java Test3 w
+            Dice Rolls before Sort
+            3 3 4 4 4 1 2 3 1 1 
+            6 3 4 6 3 6 1 3 4 2 
+            3 6 1 1 2 5 3 4 1 4 
+            2 1 6 2 1 2 2 2 1 1 
+            5 6 6 5 5 4 1 6 1 1 
+            5 4 5 4 1 1 1 6 5 6 
+            4 4 1 2 5 6 5 5 1 3 
+            6 1 5 1 1 2 1 1 3 3 
+            6 2 1 4 5 5 1 4 4 4 
+            1 4 1 6 3 1 5 4 6 2 
+            Dice Rolls after Sort
+            1 1 1 1 1 1 1 1 1 1 
+            1 1 1 1 1 1 1 1 1 1 
+            1 1 1 1 1 1 1 1 1 2 
+            2 2 2 2 2 2 2 2 2 2 
+            2 3 3 3 3 3 3 3 3 3 
+            3 3 3 4 4 4 4 4 4 4 
+            4 4 4 4 4 4 4 4 4 4 
+            4 5 5 5 5 5 5 5 5 5 
+            5 5 5 5 5 6 6 6 6 6 
+            6 6 6 6 6 6 6 6 6 6 
                 
 */
 
@@ -74,12 +106,21 @@ import java.util.Random;
 public class Test3 {
 
 
-    int[] diceRolls = new int[100];
+    static int[] diceRolls = new int[100];
     
     public static void main(String[] args) {
 
+        
         Test3.setDiceRolls(diceRolls);       
 
+        System.out.println("Dice Rolls before Sort");
+        Test3.printDiceRolls(diceRolls);
+
+
+        Test3.sortDiceRolls(diceRolls);
+
+        System.out.println("Dice Rolls after Sort");
+        Test3.printDiceRolls(diceRolls);
 
 
 
@@ -88,16 +129,17 @@ public class Test3 {
 
     public static void setDiceRolls(int[] dr) {
         Random r = new Random();
-        int c = 0,
+        int c = 0;
         int x;
 
         while (c < 100) {
 
-            x = r.nextInt(6);
+            x = r.nextInt(7);
             if (x == 0) { ++x; }
             dr[c++] = x;
 
         }
+    }
 
     public static void printDiceRolls(int[] dr) {
 
@@ -110,13 +152,45 @@ public class Test3 {
 
     }
 
+    public static void sortDiceRolls(int[] dr) {
+        int[] temp = new int[100];
+        int x=0;
+        int b=0;
+        for (int i=1; i < 7; ++i) {
+            int f = countFaceOccurence(dr, i);
+
+
+
+            for (b=0 ; b < f && x < 100; b++, x++) {
+                temp[x] = i;
+            }
+        }
+        for (int p=0; p < 100; ++p) {
+            dr[p] = temp[p];
+        }
+        return;
+
 
     }
 
+    public static int  countFaceOccurence(int[] dr, int val) {
+        int count = 0;
+        for (int i=0; i < 100; ++i) {
+            if (dr[i] == val) {
+                count++;
+            }
+        }
+        return count;
 
 
+
+    }
 
 }
+
+
+
+
 
 
 
