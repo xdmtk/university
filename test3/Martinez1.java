d5219ba74cbad9b0b02556db4fd09937caea7fd8
Nick <nick@xdmtk.org>
2018-11-21 16:34:52 -0800

    ques 4 start

--- test3/Martinez1.java
+++ test3/Martinez1.java
@@ -290,4 +290,98 @@ public class Martinez1 {
     }
 
 
+
+
+
+
+    public static void questionFour() {
+        static boolean banner = false;
+        if (!banner) {
+            System.out.println("Test 3 - Question 4:\n----------------------");   
+            int[] list = new int[8];
+            list[0] = 23;
+            list[1] = 45;
+            list[2] = 10;
+            list[3] = 2;
+            list[4] = 89;
+            list[5] = 62;
+            list[6] = 76;
+            list[7] = 33;
+            banner = true;
+        }
+
+
+       if (list.length > 1) {
+           
+           // Merge sort the first half
+           int[] firstHalf = new int[list.length / 2];
+           System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+           questionFour(firstHalf);
+       
+       
+           // Merge sort the second half
+           int secondHalfLength = list.length - list.length / 2;
+           int[] secondHalf = new int[secondHalfLength];
+           
+           System.arraycopy(list, list.length / 2,
+           secondHalf, 0, secondHalfLength);
+           questionFour(secondHalf);
+           
+           
+           // Merge firstHalf with secondHalf into list
+           questionFourMerge(firstHalf, secondHalf, list);
+       }
+       // Print the sorted list
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+    
+        System.out.println("\n");
+        
+
+
+    }
+
+
+    /** Merge two sorted lists */
+    public static void questionFourMerge(int[] list1, int[] list2, int[] temp) {
+
+
+        int current1, current2, current3;
+        current1 = current2 = current3 = 0;
+
+
+        while (current1 < list1.length && current2 < list2.length) {
+
+
+            if (list1[current1] < list2[current2])
+                temp[current3++] = list1[current1++];
+            else
+                temp[current3++] = list2[current2++];
+        }
+
+        while (current1 < list1.length)
+            temp[current3++] = list1[current1++];
+
+            while (current2 < list2.length)
+                temp[current3++] = list2[current2++];
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
