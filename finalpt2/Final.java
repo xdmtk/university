520e39bbc96a27eab73ae13935d18d849fdfb9b3
Nick Martinez <nick@xdmtk.org>
2018-12-08 10:32:08 -0800

    fofofk

--- finalpt2/Final.java
+++ finalpt2/Final.java
@@ -1,46 +1,46 @@
-    /*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
-    !                                                                            #
-    #                                                                            !
-    !   Programmer:     Nick Martinez                                            #
-    #   Class:          COSCI 436 - SAT 8:15 - 10:30AM                           !
-    !   Instructor:     Milan Samplewala                                         #
-    #   Date:           12/08/2018                                               !
-    !                                                                            #
-    #          Final Exam: Part Two                                              !
-    !                                                                            #
-    #!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+/*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+!                                                                            #
+#                                                                            !
+!   Programmer:     Nick Martinez                                            #
+#   Class:          COSCI 436 - SAT 8:15 - 10:30AM                           !
+!   Instructor:     Milan Samplewala                                         #
+#   Date:           12/08/2018                                               !
+!                                                                            #
+#          Final Exam: Part Two                                              !
+!                                                                            #
+#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
 
 
 
-________/\\\\\\\\\_______/\\\\\__________/\\\\\\\\\\\__________/\\\\\\\\\__/\\\\\\\\\\\_        
-_____/\\\////////______/\\\///\\\______/\\\/////////\\\_____/\\\////////__\/////\\\///__       
-___/\\\/_____________/\\\/__\///\\\___\//\\\______\///____/\\\/_______________\/\\\_____      
-__/\\\______________/\\\______\//\\\___\////\\\__________/\\\_________________\/\\\_____     
-_\/\\\_____________\/\\\_______\/\\\______\////\\\______\/\\\_________________\/\\\_____    
-_\//\\\____________\//\\\______/\\\__________\////\\\___\//\\\________________\/\\\_____   
-__\///\\\___________\///\\\__/\\\_____/\\\______\//\\\___\///\\\______________\/\\\_____  
-____\////\\\\\\\\\____\///\\\\\/_____\///\\\\\\\\\\\/______\////\\\\\\\\\__/\\\\\\\\\\\_ 
+________/\\\\\\\\\_______/\\\\\__________/\\\\\\\\\\\__________/\\\\\\\\\__/\\\\\\\\\\\_
+_____/\\\////////______/\\\///\\\______/\\\/////////\\\_____/\\\////////__\/////\\\///__
+___/\\\/_____________/\\\/__\///\\\___\//\\\______\///____/\\\/_______________\/\\\_____
+__/\\\______________/\\\______\//\\\___\////\\\__________/\\\_________________\/\\\_____
+_\/\\\_____________\/\\\_______\/\\\______\////\\\______\/\\\_________________\/\\\_____
+_\//\\\____________\//\\\______/\\\__________\////\\\___\//\\\________________\/\\\_____
+__\///\\\___________\///\\\__/\\\_____/\\\______\//\\\___\///\\\______________\/\\\_____
+____\////\\\\\\\\\____\///\\\\\/_____\///\\\\\\\\\\\/______\////\\\\\\\\\__/\\\\\\\\\\\_
 _______\/////////_______\/////_________\///////////___________\/////////__\///////////__
 
 
- _______________________/\\\________/\\\\\\\\\\_____________/\\\\\____________________
-  ____________________/\\\\\______/\\\///////\\\________/\\\\////____________________
- ___________________/\\\/\\\_____\///______/\\\______/\\\///___________________________
-  ________________/\\\/\/\\\____________/\\\//_____/\\\\\\\\\\\_______________________
- _______________/\\\/__\/\\\___________\////\\\___/\\\\///////\\\______________________
-  ____________/\\\\\\\\\\\\\\\\___________\//\\\_\/\\\______\//\\\____________________
- ____________\///////////\\\//___/\\\______/\\\__\//\\\______/\\\___________________
-   ____________________\/\\\____\///\\\\\\\\\/____\///\\\\\\\\\/______________________
-  _____________________\///_______\/////////________\/////////______________________
+_______________________/\\\________/\\\\\\\\\\_____________/\\\\\____________________
+____________________/\\\\\______/\\\///////\\\________/\\\\////____________________
+___________________/\\\/\\\_____\///______/\\\______/\\\///___________________________
+________________/\\\/\/\\\____________/\\\//_____/\\\\\\\\\\\_______________________
+_______________/\\\/__\/\\\___________\////\\\___/\\\\///////\\\______________________
+____________/\\\\\\\\\\\\\\\\___________\//\\\_\/\\\______\//\\\____________________
+____________\///////////\\\//___/\\\______/\\\__\//\\\______/\\\___________________
+____________________\/\\\____\///\\\\\\\\\/____\///\\\\\\\\\/______________________
+_____________________\///_______\/////////________\/////////______________________
 
 
-    Write an algorithm to convert a decimal number to binary number. Provide the Big O analysis. (5 points)
+Write an algorithm to convert a decimal number to binary number. Provide the Big O analysis. (5 points)
 
-    Then write a java program to implement the algorithm,  name the file  FinalExam.java. (5 points) 
+Then write a java program to implement the algorithm,  name the file  FinalExam.java. (5 points)
 
-    Provide the main() method to test your program. (5 points)
+Provide the main() method to test your program. (5 points)
 
-    Do not forget to run your program and show the results. 
+Do not forget to run your program and show the results.
 */
 import java.lang.Math;
 import java.util.*;
@@ -51,7 +51,7 @@ public class Final {
 
     public static void main(String[] args) {
 
-        
+
         System.out.println(Final.convertToBinaryString(3) + "\n");
 
 
@@ -59,7 +59,7 @@ public class Final {
 
 
     }
-        
+
 
 
 
@@ -67,7 +67,7 @@ public class Final {
 
         // Initialize empty string for output
         String output = "";
-        String analysis = "";  
+        String analysis = "";
         /* First determine maximum 2^n power for input number,
          * this way we can determine the limit at which to start
          * outputting binary digits
@@ -76,18 +76,20 @@ public class Final {
         base=2;
 
         /* We can do this by starting at 2^0, and working our way up
-         * 2^1, 2^2, 2^3 and so on, until our 2^n is greater than the 
+         * 2^1, 2^2, 2^3 and so on, until our 2^n is greater than the
          * input number we have received, this will tell us how many
          * binary digits we will need, since each iteration of n
          * is a binary digit of 2 to the power of the position of the binary
          * digit
          */
-        for (n=0; base < num; n++) {
+        double sum=0;
+        for (n=0; sum < num; n++) {
 
-           // We must reset the base for each iteration so we can
-           // be sure we are getting a 2^n result
-           base=2;
-           base += Math.pow(base,n);
+            // We must reset the base for each iteration so we can
+            // be sure we are getting a 2^n result
+            base=2;
+            sum = 0;
+            sum += Math.pow(base,n);
         }
 
         // Since our n has gone over by one power in order to exit the loop
@@ -97,8 +99,8 @@ public class Final {
 
 
         /* Now starting with our base 2^n, with the n that is the highest
-         * possible n that will divide into the input number, we can 
-         * begin decrementing n, and subtracting the result 2^n from the input 
+         * possible n that will divide into the input number, we can
+         * begin decrementing n, and subtracting the result 2^n from the input
          * number, but only if that 2^n is divisble by the input number
          *
          * The reason for this, is a binary number consists of the sum of the
@@ -115,8 +117,8 @@ public class Final {
             // Divide the input number by the current 2^n power
             if (num/Math.pow(base,n) >= 1) {
 
-                // If our answer is divisble, we know that that 2^n is 
-                // a '1' digit in our binary number, so we append the 1 to the 
+                // If our answer is divisble, we know that that 2^n is
+                // a '1' digit in our binary number, so we append the 1 to the
                 // string
                 output += "1";
 
@@ -133,12 +135,12 @@ public class Final {
             }
         }
 
-        
+
         System.out.println(analysis.substring(0,analysis.length()-1) + " = " + (int) numSave);
 
         // After the following algorithm has taken place, we have our binary
         // number in String form, if we really want to, we can convert this number
-        // to an integer and supply the appropriate padding, but for timessake it is 
+        // to an integer and supply the appropriate padding, but for timessake it is
         // better to leave it in string form
         return output;
 
@@ -148,7 +150,7 @@ public class Final {
 
 
 
-        
+
 
 
 
@@ -162,9 +164,3 @@ public class Final {
 
 
 }
-
-
-
-
-
-
---
+++ lab8/lab8/MinHeap.java
@@ -0,0 +1,192 @@
+    /*#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+    !                                                                            #
+    #                                                                            !
+    !   Programmer:     Nick Martinez                                            #
+    #   Class:          COSCI 436 - SAT 8:15 - 10:30AM                           !
+    !   Instructor:     Milan Samplewala                                         #
+    #   Date:           11/16/2018                                               !
+    !                                                                            #
+    #          LAB 8 - Min Heap                                                  !
+    !                                                                            #
+    #!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+
+
+
+________/\\\\\\\\\_______/\\\\\__________/\\\\\\\\\\\__________/\\\\\\\\\__/\\\\\\\\\\\_        
+_____/\\\////////______/\\\///\\\______/\\\/////////\\\_____/\\\////////__\/////\\\///__       
+___/\\\/_____________/\\\/__\///\\\___\//\\\______\///____/\\\/_______________\/\\\_____      
+__/\\\______________/\\\______\//\\\___\////\\\__________/\\\_________________\/\\\_____     
+_\/\\\_____________\/\\\_______\/\\\______\////\\\______\/\\\_________________\/\\\_____    
+_\//\\\____________\//\\\______/\\\__________\////\\\___\//\\\________________\/\\\_____   
+__\///\\\___________\///\\\__/\\\_____/\\\______\//\\\___\///\\\______________\/\\\_____  
+____\////\\\\\\\\\____\///\\\\\/_____\///\\\\\\\\\\\/______\////\\\\\\\\\__/\\\\\\\\\\\_ 
+_______\/////////_______\/////_________\///////////___________\/////////__\///////////__
+
+
+ _______________________/\\\________/\\\\\\\\\\_____________/\\\\\____________________
+  ____________________/\\\\\______/\\\///////\\\________/\\\\////____________________
+ ___________________/\\\/\\\_____\///______/\\\______/\\\///___________________________
+  ________________/\\\/\/\\\____________/\\\//_____/\\\\\\\\\\\_______________________
+ _______________/\\\/__\/\\\___________\////\\\___/\\\\///////\\\______________________
+  ____________/\\\\\\\\\\\\\\\\___________\//\\\_\/\\\______\//\\\____________________
+ ____________\///////////\\\//___/\\\______/\\\__\//\\\______/\\\___________________
+   ____________________\/\\\____\///\\\\\\\\\/____\///\\\\\\\\\/______________________
+  _____________________\///_______\/////////________\/////////______________________
+
+
+    Exercise 22.7 (Min Heap)
+
+    (Min-heap) The heap presented in the text is also known as a max-heap, in which
+    each node is greater than or equal to any of its children. A min-heap is a heap
+    in which each node is less than or equal to any of its children. Min-heaps are
+    often used to implement priority queues. Revise the Heap class in Listing 23.9 to
+    implement a min-heap.
+            
+
+
+
+
+    The following code is directly from the book: Only minor modifications were made
+    to use MinHeap
+
+    The areas where you see this in the code:
+    /******************************************************/
+/*  
+    Are areas where I have edited the code to make it a min heap
+
+
+    Output before modificaiton: 
+        xdmtk@DMT~/prog/fall2018/lab8 (master)
+        └─ $ ▶ java MinHeap 
+        -44 -5 -4 -3 0 1 1 2 3 3 4 5 53 
+
+    Output after modification
+        dmtk@DMT~/prog/fall2018/lab8 (master)
+        └─ $ ▶ java MinHeap 
+        53 5 4 3 3 2 1 1 0 -3 -4 -5 -44 
+
+
+*/
+
+public class MinHeap<E extends Comparable<E>> {
+    private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+    
+
+    /** A test method */
+    public static void main(String[] args) {
+        Integer[] list = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53};
+        heapSort(list);
+
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+
+
+
+
+
+    /** Create a default heap */
+    public MinHeap() {
+
+    }
+
+    /** Create a heap from an array of objects */
+    public MinHeap(E[] objects) {
+        for (int i = 0; i < objects.length; i++)
+            add(objects[i]);
+    }
+
+    /** Add a new object into the heap */
+    public void add(E newObject) {
+        list.add(newObject); // Append to the heap
+        int currentIndex = list.size() - 1; // The index of the last node
+
+        while (currentIndex > 0) {
+            int parentIndex = (currentIndex - 1) / 2;
+
+
+/******************************************************/
+
+            // Swap if the current object is LESS than its parent
+            if (list.get(currentIndex).compareTo(list.get(parentIndex)) < 0) {
+
+/******************************************************/
+            
+                E temp = list.get(currentIndex);
+                list.set(currentIndex, list.get(parentIndex));
+                list.set(parentIndex, temp);
+            }
+            else
+                break; // The tree is a heap now
+
+            currentIndex = parentIndex;
+        }
+    }
+
+    /** Remove the root from the heap */
+    public E remove() {
+        if (list.size() == 0) return null;
+
+        E removedObject = list.get(0);
+        list.set(0, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+
+        int currentIndex = 0;
+        while (currentIndex < list.size()) {
+            int leftChildIndex = 2 * currentIndex + 1;
+            int rightChildIndex = 2 * currentIndex + 2;
+
+            // Find the maximum between two children
+            if (leftChildIndex >= list.size()) break; // The tree is a heap
+            
+            int maxIndex = leftChildIndex;
+            if (rightChildIndex < list.size()) {
+
+/******************************************************/
+
+                if (list.get(maxIndex).compareTo(list.get(rightChildIndex)) > 0) {
+
+/******************************************************/
+                    maxIndex = rightChildIndex;
+                }
+            }
+
+            // Swap if the current node is less than the maximum
+
+/******************************************************/
+
+            if (list.get(currentIndex).compareTo(list.get(maxIndex)) > 0) {
+
+/******************************************************/
+                E temp = list.get(maxIndex);
+                list.set(maxIndex, list.get(currentIndex));
+                list.set(currentIndex, temp);
+                currentIndex = maxIndex;
+            }
+            else
+                break; // The tree is a heap
+        }
+
+        return removedObject;
+    }
+
+    /** Get the number of nodes in the tree */
+    public int getSize() {
+        return list.size();
+    }
+
+	
+	
+    public static <E extends Comparable<E>> void heapSort(E[] list) {
+        // Create a Heap of integers
+        MinHeap<E> heap = new MinHeap<>(); 
+        
+        // Add elements to the heap
+        for (int i = 0; i < list.length; i++)
+            heap.add(list[i]);
+
+        // Remove elements from the heap
+        for (int i = list.length - 1; i >= 0; i--)
+            list[i] = heap.remove(); 
+    }
+}
