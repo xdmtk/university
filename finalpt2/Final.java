5ad418bc7156f350a5b0d8f28e9d2564171bbf99
Nick <nick@xdmtk.org>
2018-12-08 10:01:33 -0800

    put comments

--- finalpt2/Final.java
+++ finalpt2/Final.java
@@ -68,29 +68,78 @@ public class Final {
 
     public static String convertToBinaryString(double num) {
 
+        // Initialize empty string for output
         String output = "";
 
-        // Determine max 2^n on num
+        /* First determine maximum 2^n power for input number,
+         * this way we can determine the limit at which to start
+         * outputting binary digits
+         */
         double base,n;
         base=2;
 
-
+        /* We can do this by starting at 2^0, and working our way up
+         * 2^1, 2^2, 2^3 and so on, until our 2^n is greater than the 
+         * input number we have received, this will tell us how many
+         * binary digits we will need, since each iteration of n
+         * is a binary digit of 2 to the power of the position of the binary
+         * digit
+         */
         for (n=0; base < num; n++) {
+
+           // We must reset the base for each iteration so we can
+           // be sure we are getting a 2^n result
            base=2;
            base += Math.pow(base,n);
         }
+
+        // Since our n has gone over by one power in order to exit the loop
+        // above, we must decrement n by 1 to accurately start computing the
+        // resulting binary digit
         --n;
 
+
+        /* Now starting with our base 2^n, with the n that is the highest
+         * possible n that will divide into the input number, we can 
+         * begin decrementing n, and subtracting the result 2^n from the input 
+         * number, but only if that 2^n is divisble by the input number
+         *
+         * The reason for this, is a binary number consists of the sum of the
+         * digits, whose position represent a 2^n, so when we subtract say 2^2
+         * from the decimal number 7, we are left with 3, so our next 2^n would
+         * be 2^1, equating to 2, which can also be subtracted by 3, and now our
+         * 2^0, is 1, can also be divided into and subtracted from 3
+         * those 3 hits mean we have a binary digit '1' in position 0,1, and 2
+         * resulting in the binary number '111'
+         */
+
         for (base=2; n >= 0; n--) {
+
+            // Divide the input number by the current 2^n power
             if (num/Math.pow(base,n) >= 1) {
+
+                // If our answer is divisble, we know that that 2^n is 
+                // a '1' digit in our binary number, so we append the 1 to the 
+                // string
                 output += "1";
+
+                // In order to accurately perform this operation for the next numbr
+                // we have to subtract the result of that 2^n from the input number
                 num -= Math.pow(base,n);
-                System.out.println("Base * n: " +  Math.pow(base,n));
             }
             else {
+
+                // If the division is not successfull, then we have a '0' digit
+                // for our binary number
                 output += "0";
             }
         }
+
+
+        // After the following algorithm has taken place, we have our binary
+        // number in String form, if we really want to, we can convert this number
+        // to an integer and supply the appropriate padding, but for timessake it is 
+        // better to leave it in string form
         return output;
 
     }
