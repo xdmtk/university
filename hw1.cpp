81b1673e3f99de8619eb2c9bfd8c0d19503c9006
Nick Martinez <nick@xdmtk.org>
2019-02-07 11:24:20 +0800

    Think i got comb

--- hw1.cpp
+++ hw1.cpp
@@ -1,21 +1,44 @@
 #include <cstdlib>
 #include <cstdio>
+#include <vector>
 
-typedef unsigned int uint;
-uint factorial(uint n);
+
+//typedef unsigned int unsigned;
+unsigned factorial(unsigned n);
+unsigned comb(unsigned n, unsigned k);
+
+
+
 
 int main() {
-    uint x = factorial(4);
+
+    unsigned x = factorial(4);
+    printf("%d\n", x);
+    x = comb(5,2);
     printf("%d\n", x);
     return 0;
 
     
 }
 
-uint factorial(uint n) {
-    uint ret=1;
+unsigned factorial(unsigned n) {
+    unsigned ret=1;
     for(int i=n; i>0; --i){
        ret *= i;
     }
     return ret;
 }
+
+unsigned comb(unsigned n, unsigned k) {
+    // For loop, start at n, traverse to 0 at interval k, decrement n, traverse to 0 add to total
+    //
+    int score = 0;
+    for ( ; n > 0; --n) {
+        for (int x=n-1; x > 0; x-=(k-1)) {
+            score++;
+        }
+    }
+    return score;
+}
+
+
