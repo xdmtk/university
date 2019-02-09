39e66ec64b9e7a2bd22d009aa30a8f08f48e9818
Nick Martinez <nick@xdmtk.org>
2019-02-08 17:25:11 -0800

    Begin val map

--- hw1.cpp
+++ hw1.cpp
@@ -1,11 +1,14 @@
 #include <cstdlib>
-#include <cstdio>
 #include <vector>
+#include <cstdio>
+#include <cassert>
 
 
 //typedef unsigned int unsigned;
 unsigned factorial(unsigned n);
 unsigned comb(unsigned n, unsigned k);
+unsigned perm(unsigned n, unsigned k);
+unsigned combABC(unsigned n);
 
 
 
@@ -13,9 +16,13 @@ unsigned comb(unsigned n, unsigned k);
 int main() {
 
     unsigned x = factorial(4);
-    printf("%d\n", x);
+    printf("Factorial 4: %d\n", x);
+
     x = comb(5,2);
-    printf("%d\n", x);
+    printf("Combinations for 5,2: %d\n", x);
+
+    x = perm(5,2);
+    printf("Permuatations for 5,2: %d\n", x);
     return 0;
 
     
@@ -30,15 +37,61 @@ unsigned factorial(unsigned n) {
 }
 
 unsigned comb(unsigned n, unsigned k) {
-    // For loop, start at n, traverse to 0 at interval k, decrement n, traverse to 0 add to total
-    //
-    int score = 0;
-    for ( ; n > 0; --n) {
-        for (int x=n-1; x > 0; x-=(k-1)) {
-            score++;
-        }
-    }
-    return score;
+    return factorial(n)/(factorial(k)*factorial(n-k));
 }
 
+unsigned perm(unsigned n, unsigned k) {
+    return factorial(n)/factorial(n-k);
+}
 
+/*
+ * combABC outputs each combination of n letters, where each letter is A, B, or C. 
+ * For example, combABC 4) would output 15 lines:
+ *
+ */
+
+unsigned combABC(unsigned n) {
+    
+    int score = 0;
+    unsigned char * perm_map = (unsigned char *) calloc(n,sizeof(unsigned char));
+    std::vector<int> val_map;
+
+    // Initialize counting
+    for (int i=0; i < n; ++i) {
+        perm_map[i] = 'A';
+    }
+    
+    score = get_comb_val(perm_map, &val_map);
+    
+    
+    
+    AAA
+    BAA
+    CAA
+    ABA
+    BBA
+    CBA
+    ACA
+    BCA
+    CCA
+    AAB
+    BAB
+    CAB
+    ABB
+    BBB
+    CBB
+    ACB
+    BCB
+    CCB
+    AAC
+    BAC
+    CAC
+    ABC
+    BBC
+    CBC
+    ACC
+    BCC
+    CCC
+
+
+}
