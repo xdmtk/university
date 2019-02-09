d74eb7bb40ee9b78d042cb7eec1e46b0358d1aee
Nick Martinez <nick@xdmtk.org>
2019-02-08 19:01:10 -0800

    Base increment works

--- hw1.cpp
+++ hw1.cpp
@@ -11,7 +11,8 @@ unsigned comb(unsigned n, unsigned k);
 unsigned perm(unsigned n, unsigned k);
 unsigned combABC(unsigned n);
 int get_comb_val(unsigned char * p_map, std::vector<int> * v_map, int n);
-
+int pow(int n,int k);
+void inc_base_n(unsigned char * perm_map, int pos);
 
 
 
@@ -25,7 +26,12 @@ int main() {
 
     x = perm(5,2);
     printf("Permuatations for 5,2: %d\n", x);
+    
+    x = combABC(4);
+    printf("Comb ABC for 4: %d\n", x);
     return 0;
+
+
 
     
 }
@@ -66,6 +72,14 @@ unsigned combABC(unsigned n) {
     
     do {
         score += get_comb_val(perm_map, &val_map, n);
+        printf("%s\n", perm_map);
+        if (perm_map[0] != 'C') {
+            perm_map[0]++;
+        }
+        else {
+            inc_base_n(perm_map, 0);
+        }
+        /*
         for (int z=0; z < n; ++z) {
             if (perm_map[z] != 'C') {
                 perm_map[z]++;
@@ -73,15 +87,24 @@ unsigned combABC(unsigned n) {
             }
             else {
                 perm_map[z] = 'A';
-                perm_map[++z]++;
+                break;
             }
         }
+        */
         rounds++;
     }
-    while (rounds < 3); // TODO: Pow 3
+    while (rounds < pow(3,n)); // TODO: Pow 3
     perm_map ? free(perm_map) : free(NULL);
     return score;
 
+}
+
+int pow(int n,int k) {
+    int val = n;
+    for (int x=0; x < k-1; ++x) {
+        n *= val;
+    }
+    return n;
 }
 
 int get_comb_val(unsigned char * p_map, std::vector<int> * v_map, int n) {
@@ -94,10 +117,23 @@ int get_comb_val(unsigned char * p_map, std::vector<int> * v_map, int n) {
     
     // Check if value is in value array
     if (std::find(v_map->begin(), v_map->end(), score) != v_map->end()) {
-       return 1;
+       return 0;
     }
-    return 0;
+    v_map->push_back(score);
+    return 1;
 }
+
+
+void inc_base_n(unsigned char * perm_map, int pos) {
+    perm_map[pos] = 'A';
+    if (perm_map[pos+1] != 'C') {
+        perm_map[pos+1]++;
+        return;
+    }
+    inc_base_n(perm_map, pos+1);
+}
+    
+
 
 
 
