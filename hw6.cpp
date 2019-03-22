9d335006e88f1122181781792a3a662acaf35258
Nick Martinez <nick@xdmtk.org>
2019-03-21 21:04:42 -0700

    hw6

--- hw6.cpp
+++ hw6.cpp
@@ -59,6 +59,73 @@
     Write a main that calls your er function. You decide the details of your main, I'm interested in your er function.
 
     Please write your program as a single .cpp file, and make the first line a comment telling me the class (CS 542), the assignment (Hw6), and your name.
+
+
+    SAMPLE OUTPUT:
+
+        [~/school/discr] (master) >>> g++ hw6.cpp
+        [~/school/discr] (master) >>>
+        [~/school/discr] (master) >>> run
+        Universe
+        * * * * * * * * * *
+        Universe  size: 10 - Show set:
+         0  1  2  3  4  5  6  7  8  9
+
+        Set
+        * * * * * * * * * *
+        Set size: 17 - Show set:
+        Show pair: (0, 5)
+        Show pair: (1, 9)
+        Show pair: (2, 1)
+        Show pair: (2, 9)
+        Show pair: (3, 0)
+        Show pair: (4, 0)
+        Show pair: (4, 2)
+        Show pair: (4, 5)
+        Show pair: (4, 6)
+        Show pair: (5, 0)
+        Show pair: (5, 5)
+        Show pair: (6, 0)
+        Show pair: (6, 1)
+        Show pair: (7, 3)
+        Show pair: (8, 3)
+        Show pair: (9, 0)
+        Show pair: (9, 6)
+
+
+        Tests
+        * * * * * * * * * * *
+        Failed reflexivity test for elements (0, 0)
+        Failed reflexivity test for elements (1, 1)
+        Failed reflexivity test for elements (2, 2)
+        Failed reflexivity test for elements (3, 3)
+        Failed reflexivity test for elements (4, 4)
+        Passed reflexivity test for elements (5, 5)
+        Failed reflexivity test for elements (6, 6)
+        Failed reflexivity test for elements (7, 7)
+        Failed reflexivity test for elements (8, 8)
+        Failed reflexivity test for elements (9, 9)
+        Passed symmetry test for elements (0, 5)
+        Failed symmetry test for elements (1, 9)
+        Failed symmetry test for elements (2, 1)
+        Failed symmetry test for elements (2, 9)
+        Failed symmetry test for elements (3, 0)
+        Failed symmetry test for elements (4, 0)
+        Failed symmetry test for elements (4, 2)
+        Failed symmetry test for elements (4, 5)
+        Failed symmetry test for elements (4, 6)
+        Passed symmetry test for elements (5, 0)
+        Passed symmetry test for elements (5, 5)
+        Failed symmetry test for elements (6, 0)
+        Failed symmetry test for elements (6, 1)
+        Failed symmetry test for elements (7, 3)
+        Failed symmetry test for elements (8, 3)
+        Failed symmetry test for elements (9, 0)
+        Failed symmetry test for elements (9, 6)
+
+
+        Failed one or more tests
+
  *
  *
 */
@@ -97,16 +164,26 @@ int main(int argc, char * argv[]) {
     // Generate ordered pairs and 
     // 1. Push to SOP `mySop`
     // 2. Insert each element into universal set `universe`
-    for (int x=0; x < 5; x++) {
+    for (int x=0; x < 20; x++) {
         
-        OP test_mp = makeOP((unsigned) rand() % 30, (unsigned) rand() % 30);
+        OP test_mp = makeOP((unsigned) rand() % 10, (unsigned) rand() % 10);
 
         mySop.insert(test_mp);
         universe.insert(test_mp.first);
         universe.insert(test_mp.second);
     }
+    printf("Universe\n* * * * * * * * * *");
+    show(universe);
+    
+    printf("\n\nSet\n* * * * * * * * * *");
+    show(mySop);
+    printf("\n\nTests\n* * * * * * * * * * *\n");
+    if (er(mySop, universe)) {
+        printf("\n\nPassed all three tests\n");
+        return 0;
+    }
+    printf("\n\nFailed one or more tests\n");
 
-    er(mySop, universe);
 }
 
 
@@ -116,7 +193,7 @@ int main(int argc, char * argv[]) {
  *  Transitivity: for any a, b, and c in the universal set, if (a, b) and (b, c) are in the relation, then (a, c) is also in the relation.
  */
 bool er(const SOP & sop, const std::set<unsigned> & univ) {
-    
+    bool fail_flag = false; 
     // Begin iterating the universe and applying tests to elements
     std::set<unsigned>::iterator it = univ.begin();
 
@@ -129,9 +206,12 @@ bool er(const SOP & sop, const std::set<unsigned> & univ) {
         // Reflexive test , make OP (elem,elem) and test whether than OP 
         // is in the set `sop`
         if (sop.find( makeOP( elem, elem )) == sop.end()) { 
-            return false;
+            printf("Failed reflexivity test for elements (%d, %d)\n", elem, elem);
+            fail_flag = true;
+            continue;
         }
         // If OP (elem , elem) is in `sop`. continue with tests
+        printf("Passed reflexivity test for elements (%d, %d)\n", elem, elem);
     }
 
 
@@ -147,8 +227,11 @@ bool er(const SOP & sop, const std::set<unsigned> & univ) {
         // Make an OP with `elemA` and `elemB` reversed and check
         // for existence in `sop`
         if (sop.find( makeOP( elemB , elemA )) == sop.end()) { 
-            return false;
+            printf("Failed symmetry test for elements (%d, %d)\n", elemA, elemB);
+            fail_flag = true;
+            continue;
         }
+            printf("Passed symmetry test for elements (%d, %d)\n", elemA, elemB);
     }
 
 
@@ -156,8 +239,6 @@ bool er(const SOP & sop, const std::set<unsigned> & univ) {
     // For this test, the pair making operation is a bit more expensive,
     // as we must make (n!/(3! (n - 3)!)) OP's
     //
-    show(univ);
-
     // First we get the size `n` from the universal set
     int universe_len = univ.size();
 
@@ -179,16 +260,19 @@ bool er(const SOP & sop, const std::set<unsigned> & univ) {
             // Now we can ask, well is (a, c) in the set as well? And if not, return false
             // and the set has failed the transivity test
             if (sop.find(makeOP(a, c)) == sop.end()) {
-                return false;
+                printf("Failed transitivity test for elements A: %d B: %d C: %d\n", a, b, c);
+                fail_flag = true;
+                continue;
             }
-
+            printf("Passed transitivity test for elements A: %d B: %d C: %d\n", a, b, c);
         }
-        
         
     } while (next_combination(universe_len, 3, choose_k));
     
-
-
+    
+    // If all 3 tests have not returned false, er has passed and will
+    // return true
+    return !fail_flag;
 }
 
 // Generates the combinations for 3 values (a, b, and c,) (k) in the
@@ -212,9 +296,9 @@ bool next_combination(int n, int r, int *a) {
 
 void assign_values(int *arr, int n, unsigned *a, unsigned *b, unsigned *c) {
 
-    arr[0] = *a;
-    arr[1] = *b;
-    arr[2] = *c;
+    *a = arr[0];
+    *b = arr[1];
+    *c = arr[2];
 
 }
 
@@ -232,9 +316,9 @@ OP makeOP(unsigned first, unsigned second) {
 void show(std::set<unsigned> u) {
    
     std::set<unsigned>::iterator it;
-    printf("\nSet size: %d - Show set:\n", u.size());
+    printf("\nUniverse  size: %d - Show set:\n", u.size());
     for (it = u.begin(); it != u.end(); it++) { 
-        printf("%d", *it);
+        printf(" %d ", *it);
     }
 
 
