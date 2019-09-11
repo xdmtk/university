57e86c8cda2ea721273a3e411b58c2e0daaac01a
Nick Martinez <nick@xdmtk.org>
2019-09-11 11:45:27 -0700

    Add enter parameters function

--- 1/main.c
+++ 1/main.c
@@ -12,13 +12,19 @@ struct state {
 
 int show_menu(void);
 void handle_selection(int selection, struct state * st);
-void invalid_selection();
+void enter_parameters(struct state * st);
+void invalid_selection(struct state * st);
 
 
 int main(void) {
     
     int selection;
-    struct state * st = (struct state *)malloc(sizeof(struct state));
+    struct state * st;
+
+    if (!(st = (struct state *)malloc(sizeof(struct state)))) {
+        printf("\nCould not allocate memory. Exiting!\n");
+        exit(-1);
+    }
 
 
     while (1) {
@@ -32,16 +38,48 @@ void handle_selection(int selection, struct state * st) {
 
     switch (selection) {
         case 1:
+            enter_parameters(st);
             break;
         case 2:
             break;
         case 3:
             break;
         default:
-            invalid_selection();
+            invalid_selection(st);
             break;
     }
 }
+
+
+void enter_parameters(struct state * st) {
+    
+    char * items[] = {
+        "\n\nEnter the frequency of the machine (MHz): ",
+        "\n\nEnter the number of instruction classes: ",
+        "\n\nEnter CPI of class "
+    };
+
+    printf("%s", items[1]);
+    scanf("%d", &st->frequency);
+
+    printf("%s", items[2]);
+    scanf("%d", &st->instruction_classes);
+
+    if (!(st->ic = (struct instruction_class *) malloc(sizeof(struct instruction_class *)
+            *st->instruction_classes))) {
+        printf("\nCould not allocate memory. Exiting!\n");
+        free(st);
+        exit(-1);
+    }
+    
+
+
+
+
+
+
+}
+
 
 
 int show_menu(void) {
