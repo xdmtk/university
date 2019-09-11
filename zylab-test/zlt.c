d2c92d8ebc22321bbd1f66d9d8ac04432dc4be26
Nick Martinez <nick@xdmtk.org>
2019-09-10 23:53:59 -0700

    Initial commit for Zylab test

---
+++ zylab-test/zlt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int x,y;
+
+    /* Print out header */
+    printf("Please enter sets of two integers:\n\n"
+            "X\tY\tZ\n\n");
+
+    /* As long as there is continued input, */
+    while (scanf("%d\t%d", &x, &y) >= 0){
+
+        /* Print out x, y, and their sum */
+        printf("%d\t%d\t%d\n\n",x,y,x+y);
+
+        /* If their sum is 0, we can break the loop
+         * and terminate the program */
+        if (!(x+y))
+            break;
+    }
+    return 0;
+}
+
