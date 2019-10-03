8ccf60e98214a5c655d224c4097d6bbe60ad5c61
Nick Martinez <nick@xdmtk.org>
2019-10-03 11:51:41 -0700

    Modularize the char writeout functions

--- 2/Assignment_2.c
+++ 2/Assignment_2.c
@@ -10,6 +10,9 @@ int show_menu(void);
 void handle_selection(int selection);
 int get_unbiased_exp(float dec_in);
 void exit_program(void);
+void write_mantissa(unsigned char *mantissa, float mantissa_float);
+void write_exponent(float *dec_in, unsigned char * biased_exp, int * unbiased_exp, int sign);
+void write_sign(float dec_in, unsigned char *sign);
 
 
 /* Main function */
@@ -24,35 +27,76 @@ int main(void) {
 void decimal_to_floating(void) {
     
     float dec_in;
-    int i, remainder, biased_exp_int, unbiased_exp;
+    int unbiased_exp;
     unsigned char biased_exp[8], mantissa[23], hex[8], sign[1];
     
     /* Input decimal */
     printf(_spc_"Enter the decimal representation:");
+
+    /* Bad input? Exit the program */
     if (!scanf("%f",&dec_in))
         exit_program();
     
-    /* Calculate sign bit */
+    /* Write out the sign bit */
+    write_sign(dec_in, sign);
+    
+    /* Write out the exponent bits */
+    write_exponent(&dec_in, biased_exp, &unbiased_exp, sign[0] == '1' ? -1 : 1);
+    
+    /* Write out the mantissa bits */
+    write_mantissa(mantissa, dec_in/pow(2,unbiased_exp) - 1.0);
+
+}
+
+void write_sign(float dec_in, unsigned char *sign) {
     sign[0] = dec_in < 0 ? '1' : '0';
-    
+}
+
+
+
+void write_exponent(float *dec_in, unsigned char * biased_exp, int * unbiased_exp, int sign) {
+    int i, biased_exp_int;
+
     /* Calculate integer representation of biased exponet */
-    biased_exp_int = 127 + (unbiased_exp = get_unbiased_exp(dec_in));
-    
-    /* Calucate the remainder */
-    remainder = dec_in - pow(2,unbiased_exp);
+    biased_exp_int = 127 + (*unbiased_exp = get_unbiased_exp(*dec_in));
+   
+    /* Make positive for sake of calculation */
+    *dec_in *= sign;
     
     /* Convert biased exponet to binary char array */
     for (i = 7; i >= 0; i--) {
         biased_exp[i] = (biased_exp_int & 0x1) ? '1' : '0';
         biased_exp_int = biased_exp_int >> 1;
     }
-    
-    
-        
 
 
 
+}
 
+
+void write_mantissa(unsigned char *mantissa, float mantissa_float) {
+
+    int i;
+    /* Convert mantisa decimal representation to binary char array */
+    for (i = 0; i < 23; i++) {
+
+        /* Get value of 2^(current_bit) */
+        float pow_res = pow(2,-1 - i);
+
+        /* Subtract from current value of mantissa float representation */
+        float res = mantissa_float - pow_res;
+
+        /* If result is still 0 or greater, mark a 1 on the current
+         * mantissa bit, and set the mantissa float representation
+         * to the result of the subtraction 
+         */
+        if (res >= 0) {
+            mantissa[i] = '1';
+            mantissa_float = res;
+        }
+        else
+            mantissa[i] = '0';
+    }
 
 }
 
