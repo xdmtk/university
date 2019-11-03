31a8e145d70195d2182644741fcebc8661966368
Nick Martinez <nick@xdmtk.org>
2019-11-03 11:52:37 -0800

    Add comments and separate helper method

--- arraysorts/src/ArraySorts.java
+++ arraysorts/src/ArraySorts.java
@@ -12,26 +12,16 @@ class ArraySorts {
 
     public static void insertionSortIterative(int a[], int n) {
 
-        /* Base case */
+        /* Base case  - Do nothing */
         if (n <= 1) {}
 
-        /* Iterative Insertion Sort */
+        /* Otherwise begin iterative Insertion Sort */
         else
             /* Begin at element 1, compare with previous element */
             for (int i = 1; i < n; ++i)
 
                 /* Previous element bigger? Initiate shift */
-                if (a[i] < a[i-1])
-
-                    /* Begin iterating in reverse from current element */
-                    for (int j = i; j > 0; --j)
-
-                        /* Swap elements successively ( shift ) until previous element is smaller */
-                        if (a[j] < a[j-1])  Helpers.swap(a, j, j-1);
-
-                        /* No need to iterate further after reaching smaller previous element, already
-                         * sorted */
-                        else break;
+                if (a[i] < a[i-1]) Helpers.shift(a, i, 0);
     }
     public static void insertionSort(int a[], int n) {
 
@@ -68,10 +58,42 @@ class pair {
 
 
 class Helpers {
-    public static void swap(int a[], int first, int second) {
+
+    /**
+     * Reusable in-place swap function. XOR's elements a[first] and a[second]
+     * against each other repeatedly to swap values
+     *
+     * @param a - Array to operate on
+     * @param first - First value
+     * @param second - Second value
+     */
+    static void swap(int a[], int first, int second) {
         a[first] ^= a[second];
         a[second] ^= a[first];
         a[first] ^= a[second];
+    }
+
+    /**
+     * Reusable shift function. Shifts lesser elements left
+     * by continuously swapping until left element is less than the
+     * current element
+     *
+     * @param a - Array to operate on
+     * @param start - Index of element to shift
+     * @param end - Index of limiting slice of a[]
+     *
+     */
+    static void shift(int a[], int start, int end) {
+
+        /* Begin iterating in reverse from current element */
+        for (int j = start; j > end; --j)
+
+            /* Swap elements successively ( shift ) until previous element is smaller */
+            if (a[j] < a[j - 1]) Helpers.swap(a, j, j - 1);
+
+            /* No need to iterate further after reaching smaller previous element, already
+             * sorted */
+            else break;
     }
 }
 
