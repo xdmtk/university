78903330083ddf4edb7e9a28218428426a278332
Nick Martinez <nick@xdmtk.org>
2019-11-03 13:25:04 -0800

    Finish outside in partition, fix XOR swap bug

--- arraysorts/src/ArraySorts.java
+++ arraysorts/src/ArraySorts.java
@@ -2,36 +2,25 @@ import java.math.*;
 import java.util.Random;
 
 class ArraySorts {
+    public static boolean debug = true;
+
 
     public static void main(String[] args) {
-        int a[] = UnitTests.generateRandomDataSet(100, 100);
-        insertionSort(a,a.length);
-        //insertionSortIterative(a,a.length);
-        UnitTests.prettyPrintArray(a,a.length);
+        int a[] = UnitTests.generateRandomDataSet(2, 100);
+        partitionOutsideIn(a,0,a.length-1);
     }
 
-    public static void insertionSort(int a[], int n) {
-        insertionSortRecursive(a, n);
-    }
+
+
 
     /**
-     * Iterative approach to Insertion Sort, requires no driver method
+     * Driver method for recursive insertion sort.
      *
      * @param a - Array to operate on
      * @param n - Length of array
      */
-    public static void insertionSortIterative(int a[], int n) {
-
-        /* Base case  - Do nothing */
-        if (n <= 1) {}
-
-        /* Otherwise begin iterative Insertion Sort */
-        else
-            /* Begin at element 1, compare with previous element */
-            for (int i = 1; i < n; ++i)
-
-                /* Previous element bigger? Initiate shift */
-                if (a[i] < a[i-1]) Helpers.shift(a, i, 0);
+    public static void insertionSort(int a[], int n) {
+        insertionSortRecursive(a, n);
     }
 
     /**
@@ -62,10 +51,85 @@ class ArraySorts {
         }
     }
 
+    /**
+     * Iterative approach to Insertion Sort, requires no driver method
+     *
+     * @param a - Array to operate on
+     * @param n - Length of array
+     */
+    public static void insertionSortIterative(int a[], int n) {
+
+        /* Base case  - Do nothing */
+        if (n <= 1) {}
+
+        /* Otherwise begin iterative Insertion Sort */
+        else
+            /* Begin at element 1, compare with previous element */
+            for (int i = 1; i < n; ++i)
+
+                /* Previous element bigger? Initiate shift */
+                if (a[i] < a[i-1]) Helpers.shift(a, i, 0);
+    }
+
+
+
+
 
     public static void QuickSort1(int a[], int n, int cutoff) {
 
     }
+
+
+    /**
+     * Outside-in partitioning function. Random pivot is chosen, and left/right pointers
+     * are moved successively from the outer bounds of the array, until they overlap,
+     * swapping elements when greater/less conditions against the pivot are met.
+     *
+     * @param a - Array to operate on
+     * @param left - Left index of array slice
+     * @param right - Right index of array slice
+     *
+     * @return - pair - New pair of the modified left/right indices, with 'right' as the
+     * outer bound of the left partition, and 'left' as the beginning index for the right partition
+     */
+    public static pair partitionOutsideIn(int a[], int left, int right)  {
+
+        int pivot;
+        Random randomGen;
+
+        /* Base case - Do nothing */
+        if (right - left < 1) {}
+
+        /* Otherwise begin partitioning */
+        else {
+
+            randomGen = new Random();
+
+            /* Get the pivot by generating a random index within the bounds of right - left,
+             * then add left as the base index for the chosen random value  */
+            pivot = a[left + randomGen.nextInt(right - left)];
+
+            if (ArraySorts.debug) System.out.println("Pivot: " + pivot);
+
+            /* Begin swapping elements until the left/right pointers overlap */
+            while (left < right) {
+
+                if (ArraySorts.debug) UnitTests.prettyPrintArray(a,a.length);
+
+                /* Begin incrementing/decrementing left/right pointers until elements
+                 * greater/less than the pivot are identified. */
+                while (left < right && a[left] < pivot)
+                    left++;
+                while (right > left && a[right] > pivot)
+                    right--;
+
+                Helpers.swap(a, left, right);
+            }
+        }
+        return new pair(right,left);
+    }
+
+
     public static void HeapSortBU(int a[], int n){
 
     }
@@ -105,9 +169,11 @@ class Helpers {
      * @param second - Second value
      */
     static void swap(int a[], int first, int second) {
-        a[first]  ^= a[second];
-        a[second] ^= a[first];
-        a[first]  ^= a[second];
+        if (a[first] != a[second]) {
+            a[first] ^= a[second];
+            a[second] ^= a[first];
+            a[first] ^= a[second];
+        }
     }
 
     /**
