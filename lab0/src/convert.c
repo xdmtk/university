7b37c7dddaf7dd59d046008ef160d69e5591715f
Nick Martinez <nick@xdmtk.org>
2020-01-25 15:49:47 -0800

    Implement binary string get function

--- lab0/src/convert.c
+++ lab0/src/convert.c
@@ -43,9 +43,13 @@ struct int_rep **convert_ints_to_rep(int *int_list, size_t int_count) {
 
     rep = malloc(sizeof(struct int_rep *)*int_count);
     for (i = 0; i < int_count; ++i)  {
+
         rep[i] = malloc(sizeof(struct int_rep));
+
         rep[i]->val = int_list[i];
         rep[i]->parity = get_parity_str(rep[i]->val);
+        rep[i]->original_string = get_bin_str(rep[i]->val);
+        rep[i]->ascii_rep = get_ascii_rep(rep[i]->val);
     }
     return rep;
 }
@@ -56,11 +60,26 @@ char * get_ascii_rep(int val) {
 
 char * get_bin_str(int val) {
 
+    char * bin_string;
+    int i;
+
+    bin_string = malloc(sizeof(char)*TOKEN_SIZE);
+
+    /* Walk the bits of the given value, insert a '1' character for each 1 bit */
+    for (i = 0; i < TOKEN_SIZE; ++i)
+        bin_string[i] = (val >> i) & 1 ? '1' : '0';
+
+    return bin_string;
 }
 
 char * get_parity_str(int val) {
+
     int bit_count, i;
+
+    /* Walk the bits of the given value, increment a counter for each 1 bit */
     for (bit_count = i = 0; i < TOKEN_SIZE; ++i)
         bit_count += (val >> i) & 1;
+
+    /* Return an EVEN/ODD string depending on the bit count */
     return bit_count % 2 ? "ODD" : "EVEN";
 }
