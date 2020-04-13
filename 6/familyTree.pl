522009052c4c4847e21a85485ce9b920f06d2a4b
Nick Martinez <nick@xdmtk.org>
2020-04-12 21:55:06 -0700

    Add more pl

--- 6/familyTree.pl
+++ 6/familyTree.pl
@@ -1,27 +1,83 @@
+/*
+ * Rules 
+ */
+
+/* Women */
 female(evangeline).
 female(gloria).
 female(lynette).
 female(ariel).
+female(maria).
+female(natalie).
+female(laura).
+female(karen).
 
+/* Men */
 male(nick).
 male(harold).
 male(gonzalo).
 male(ben).
+male(richard).
+male(jeff).
+male(leonard).
 
+/* Parents */
+% Parents
 parent(harold, nick).
 parent(harold, ariel).
 parent(lynette, nick).
 parent(lynette, ariel).
 
+% Grandparents (Dads side)
 parent(gonzalo,harold).
 parent(evangeline,harold).
+parent(gonzalo,leonard).
+parent(evangeline,leonard).
+parent(gonzalo,laura).
+parent(evangeline,laura).
+parent(gonzalo,karen).
+parent(evangeline,karen).
+
+% Grandparents (Moms side)
 parent(ben,lynette).
 parent(gloria,lynette).
+parent(ben,natalie).
+parent(gloria,natalie).
+
+% GreatGrandparents (Moms Side)
+parent(richard,gloria).
+parent(maria,gloria).
+
+% Great Grandparents (Dads side)
+
+
 
 mother(X,Y) :- female(X),parent(X,Y).
 father(X,Y) :- male(X),parent(X,Y).
 grandparent(X,Y) :- parent(X,Z),parent(Z,Y).
+
 ancestor(X,Y) :- parent(X,Y).
 ancestor(X,Y) :- parent(X,Z), ancestor(Z,Y).
+
 descendant(X,Y) :- parent(Y,X).
 descendant(X,Y) :- parent(Y,Z), descendant(X,Z).
+
+grandfather(X,Y) :- male(X),parent(X,Z),parent(Z,Y).
+grandmother(X,Y) :- female(X),parent(X,Z),parent(Z,Y).
+
+greatGrandfather(X,Y) :-
+    male(X),parent(X,W),parent(W,Z),parent(Z,Y).
+greatGrandmother(X,Y) :-
+    female(X),parent(X,W),parent(W,Z),parent(Z,Y).
+
+child(X,Y) :- parent(Y,X).
+
+siblings(X,Y) :- child(X,Z),child(Y,Z),X \= Y.
+
+brother(X,Y) :- siblings(X,Y),male(X).
+
+sister(X,Y) :- siblings(X,Y),female(X).
+
+aunt(X,Y) :- siblings(X,Z),parent(Z,Y),female(X).
+
+uncle(X,Y) :- siblings(X,Z),parent(Z,Y),male(X).
---
+++ mt2/Problem8.java
@@ -0,0 +1,34 @@
+
+
+
+import java.util.*;
+
+
+public class Problem8
+{
+    public static int f (int [] r) 
+    {
+        int k = r.length/2;
+        r[k] = r[k] + 1;
+        return r[k];
+    }
+
+    public static void main(String[] args)
+    {
+        double[] x = {1.0 , 2.0, 3.0, 4.0};
+        int[] a = {5, 6, 7, 8, 9};
+        double[] w = x + a;
+        int b = f(a);
+        double u = f(a);
+        int y = f(x);
+        double[] z = new double[6];
+        z = x;
+        double d = 3.1415;
+        int c = 1 + c*d;
+        int d = 3+5*6/7%3;
+        System.out.println(d);
+    }
+}
+
+
+
