254a473aeccc657b831ddde2ad487bdac864bd3e
Nick Martinez <nick@xdmtk.org>
2020-04-23 11:59:11 -0700

    2

--- 7/7.pl
+++ 7/7.pl
@@ -1,3 +1,10 @@
+% Problem 1 - Min3
+
 min3(X,Y,Z,R) :-  X =< Y, X =< Z, R = X.
 min3(X,Y,Z,R) :-  Y =< X, Y =< Z, R = Y.
 min3(X,Y,Z,R) :-  Z =< Y, Z =< X, R = Z.
+
+% Problem 2 - maxList(L,R)
+maxList([H], R) :- R = H.
+maxList([H|T], R) :- maxList(T,R), H > R, H = R.
+maxList([H|T], R) :- maxList(T,R), H =< R, T = R.
