3c76a173100064156b4662f27ce1f819f330a36c
Nick Martinez <nick@xdmtk.org>
2020-05-06 11:43:40 -0700

    Add mt3

--- 7/7.pl
+++ 7/7.pl
@@ -1,7 +1,9 @@
-magicSquare(X1, X2, X3, X4, X5, X6, X7, X8, X9):-
-
-	permutation([1,2,3,4,5,6,7,8,9], [X1, X2, X3, X4, X5, X6, X7, X8, X9]),
-
-	X1 + X2 + X3 =:= 15, X4 + X5 + X6 =:= 15, X7 + X8 + X9 =:= 15, 
-    X1 + X4 + X7 =:= 15, X2 + X5 + X8 =:= 15, X1 + X5 + X9 =:= 15,
-    X1 + X5 + X9 =:= 15, X3 + X5 + X7 =:= 15.
+maxList([H], R) :- R = H.
+maxList([H|T],R):-
+    maxList(T,TMax),
+    H > TMax,
+    R = H.
+maxList([H|T],R):-
+    maxList(T,TMax),
+    H =<TMax,
+    R = TMax.
