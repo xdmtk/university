50e6188dbb1840674854ec72f0f3ced756713acf
Nick Martinez <nick@xdmtk.org>
2020-09-21 08:53:20 -0700

    #6 - Initial commit on connector

--- include/chat/connector.h
+++ include/chat/connector.h
@@ -1,14 +1,24 @@
 #ifndef CHAT_CONNECTOR_H
 #define CHAT_CONNECTOR_H
+#include <netinet/in.h>
 #include <chat/defs.h>
+
+#include <utility>
 
 class Connector {
 public:
     Connector(ChatFacade * chat);
+
     bool connectToClient(std::string address, std::string port);
+    std::string getFailureReason() {return reason;}
 
 private:
     ChatFacade * chat;
+    std::string reason;
+
+    void setFailureReason(std::string r) {reason = std::move(r);}
+    int validateConnectionParameters(std::string address,
+            std::string port, struct sockaddr_in * s);
 };
 
 
--- src/connector.cpp
+++ src/connector.cpp
@@ -1,4 +1,9 @@
+#include <chat/defs.h>
 #include <chat/connector.h>
+#include <chat/client.h>
+
+#include <arpa/inet.h>j
+#include <utility>
 
 Connector::Connector(ChatFacade *chat) {
     this->chat = chat;
@@ -6,6 +11,48 @@ Connector::Connector(ChatFacade *chat) {
 
 bool Connector::connectToClient(std::string address, std::string port) {
 
+    struct sockaddr_in destination;
+    int outgoingSocket, outgoingPort;
 
+    if (!(outgoingPort = validateConnectionParameters(std::move(address), std::move(port),
+            &destination))) {
+        return false;
+    }
+
+    if ((outgoingSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        setFailureReason("Could not create new socket!");
+        return false;
+    }
+
+    destination.sin_family = AF_INET;
+    destination.sin_port = outgoingPort;
+
+    if (connect(outgoingSocket, (struct sockaddr *)&destination, sizeof(destination)) < 0) {
+        setFailureReason("Could not connect to destination!");
+        return false;
+    }
+
+    chat->clientVector->emplace_back(
+            new Client(chat->server, outgoingSocket, outgoingPort));
+
+    return true;
 }
 
+
+int Connector::validateConnectionParameters(std::string address, std::string port,
+        struct sockaddr_in * s) {
+
+    if (inet_pton(AF_INET, address.c_str(), &s->sin_addr) <= 0) {
+        setFailureReason("Invalid address!");
+        return 0;
+    }
+
+    try {
+        return std::stoi(port);
+    }
+    catch (std::exception &e) {
+        setFailureReason("Invalid port number!");
+        return 0;
+    }
+
+}
