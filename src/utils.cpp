3fc7238e63acfafb28dbf1379c21f6e25971db28
Nick Martinez <nick@xdmtk.org>
2020-09-21 21:31:52 -0700

    #7 - Minor typo;

--- src/utils.cpp
+++ src/utils.cpp
@@ -45,7 +45,7 @@ inline void trim(std::string &s) {
 std::string collapseTokens(std::vector<std::string> tokens, int begin, int end) {
 
     std::string out;
-    for (int i = begin; int i < end; ++i) {
+    for (int i = begin; i < end; ++i) {
         out += tokens[i] + " ";
     }
     return out.substr(0, out.size()-1);
